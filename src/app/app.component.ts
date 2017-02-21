d4a4a5b4ab51f7b394d9dfb1d48e2e0242ea4f43
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-21 00:01:00 -0500

    finish initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html'
 })
 export class AppComponent {
- 
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+
+  onDelete(item) {
+    this.list.splice(this.list.indexOf(item), 1);
+  }
 }
