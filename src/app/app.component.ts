7e9a5a0b9ce268e7e8d986ff1faf6c6e0cb9a240
Dmitry Harnitski <dharnitski@gmail.com>
2017-02-21 00:52:26 -0500

    The "void" State

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,26 @@ import { Component, trigger, state, style, transition, animate } from '@angular/
         })),
         animate(500)
       ])
-    ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        'opacity': 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300,  style({
+          opacity: 0,
+          transform: 'translateX(100px)'
+        }),)
+      ]),
+    ]),
   ]
 })
 export class AppComponent {
